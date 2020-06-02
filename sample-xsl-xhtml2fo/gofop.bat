fd6e989580421c9fe730776a30c219ef27fd3505
Joel Caples <jmcaples@gmail.com>
2020-06-01 20:51:41 -0500

    added old files

---
+++ sample-xsl-xhtml2fo/gofop.bat
@@ -0,0 +1,5 @@
+C:\apps\fop\fop.bat ^
+-xml C:\dev\jDev\sample-xsl-xhtml2fo\Howtodevelop-en-2a\xml\Howtodevelop-en-2a.xml ^
+-xsl C:\dev\jDev\sample-xsl-xhtml2fo\Howtodevelop-en-2a\SimpleDoc\article.xsl ^
+-pdf C:\dev\jDev\sample-xsl-xhtml2fo\Howtodevelop-en-2a\results\article.pdf
+
